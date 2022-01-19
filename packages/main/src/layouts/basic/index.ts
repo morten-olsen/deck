0b3cbc586df59a6ec62d3fc594eebcb587fef85c
Morten Olsen <morten+pgp@olsen.pro>
2022-01-19 22:16:08 +0100

    refactor: add createBack

--- packages/main/src/layouts/basic/index.ts
+++ packages/main/src/layouts/basic/index.ts
@@ -1,21 +1,6 @@
 import { Button } from '../../App';
-import { createCanvas } from '../../images';
 import { Layout, PositionArgs } from '../../Layout';
-
-const backImage = createCanvas((canvas) => {
-  const width = canvas.width * 0.3;
-  const height = canvas.width * 0.6;
-  const centerX = canvas.width / 2 - 20;
-  const centerY = canvas.width / 2;
-  canvas.setStrokeWidth(30);
-  canvas.setStroke('#666');
-  canvas.beginPath()
-  canvas.moveTo(centerX + width / 2, centerY - height / 2)
-  canvas.lineTo(centerX - width / 2, centerY)
-  canvas.lineTo(centerX + width / 2, centerY + height / 2)
-  canvas.stroke();
-  return canvas;
-});
+import { createBack } from '../../images';
 
 class BasicLayout<TButton extends Button> implements Layout<TButton> {
   #backButton?: Button;
@@ -29,7 +14,7 @@ class BasicLayout<TButton extends Button> implements Layout<TButton> {
     const buttons: (TButton | Button)[] = [];
     if (!this.#backButton) {
       this.#backButton = new Button({
-        image: backImage,
+        image: createBack(),
         action: api.back,
       })
     }
